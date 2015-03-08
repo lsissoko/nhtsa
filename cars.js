38b1ffc5e9cabf1004c3a4618fe4c37a3a5eaca8
Lamine Sissoko <lsissoko91@gmail.com>
2015-03-07 23:38:16 -0500

    code cleanup

--- cars.js
+++ cars.js
@@ -1,155 +1,145 @@
-var main = function() {
-  
+$(document).ready(function() {
+
+    String.prototype.squish = function() {
+        return this.replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g, "").replace(/\s+/g, " ");
+    };
+
+  function years(data) {
+    $("#results").empty();
+
+    $("<tr>")
+      .html("<td><strong>year</strong></td><td><strong>make</strong></td>")
+      .appendTo("#results");
+
+    $.each(data.Results, function(key, val) {
+      $("<tr>").html(
+        "<td>" + val.ModelYear + "</td>" +
+        "<td>" + val.Make + "</td>"
+      ).appendTo($("#results"));
+    });
+  }
+
+
+  function year_make(data) {
+    $("#results").empty();
+
+    $("<tr>")
+      .html("<td><strong>year</strong></td><td><strong>make</strong></td><td><strong>model</strong></td>")
+      .appendTo("#results");
+
+    $.each(data.Results, function(key, val) {
+      $("<tr>").html(
+        "<td>" + val.ModelYear + "</td>" +
+        "<td>" + val.Make + "</td>" +
+        "<td>" + val.Model + "</td>"
+      ).appendTo($("#results"));
+    });
+  }
+
+
+  function year_make_model(data) {
+    $("#results").empty();
+
+    $("<tr>").html("<td><strong>id</strong></td>" +
+      "<td>&nbsp;&nbsp;</td>" +
+      "<td><strong>year/make/model</strong></td>"
+    ).appendTo("#results");
+
+    $.each(data.Results, function(key, val) {
+      $("<tr id='" + val.VehicleId + "'>").html(
+        "<td>" + val.VehicleId + "</td>" +
+        "<td>&nbsp;&nbsp;</td>" +
+        "<td>" + val.VehicleDescription + "</td>"
+      ).appendTo($("#results"));
+    });
+  }
+
+
+  function rating(data) {
+    $("#results").empty();
+
+    $("<tr>").html("<td><strong>year/make/model</strong></td>" +
+      "<td>&nbsp;&nbsp;</td>" +
+      "<td><strong>rating</strong></td>"
+    ).appendTo("#results");
+
+    $.each(data.Results, function(key, val) {
+      $("<tr>").html(
+        "<td>" + val.VehicleDescription + "</td>" +
+        "<td>&nbsp;&nbsp;</td>" +
+        "<td>" + val.OverallRating + "</td>"
+      ).appendTo($("#results"));
+    });
+  }
+
+
   $("#search_btn_id").click(function(e) {
     e.preventDefault();
-    
-    var year = $(":input[name=year]").val();
-    var make = $(":input[name=make]").val();
-    var model = $(":input[name=model]").val();
-    
-    if (!((year === "") && (make === "" && model != ""))) {
+
+    var year = $(":input[name=year]").val().squish();
+    var make = $(":input[name=make]").val().squish();
+    var model = $(":input[name=model]").val().squish();
+
+    // Step 1 works if the following fields are filled:
+    // - year
+    // - year, make
+    // - year, make, model
+    var valid = true;
+    if (year === "") {
+      valid = false;
+    } else {
+      if (make === "" && model !== "") {
+        valid = false;
+      }
+    }
+
+    if (valid) {
       $("#search_btn_id").removeClass("disabled");
-      
-      var func = "year";
+
+      var successFunction = years;
       var apiParam = "/modelyear/" + year;
-      if (make != "") {
-        apiParam += "/make/" + make
-        func = "year_make";
+      if (make !== "") {
+        apiParam += "/make/" + make;
+        successFunction = year_make;
       }
-      if (model != "") {
-        apiParam += "/model/" + model
-        func = "year_make_model";
+      if (model !== "") {
+        apiParam += "/model/" + model;
+        successFunction = year_make_model;
       }
-      
+
       $.ajax({
-          type: "GET",
-          url: "http://www.nhtsa.gov/webapi/api/SafetyRatings" + apiParam + "?format=json",
-          dataType: "jsonp",
-          success: process
+        type: "GET",
+        url: "http://www.nhtsa.gov/webapi/api/SafetyRatings" + apiParam + "?format=json",
+        dataType: "jsonp",
+        success: function (data) {
+          successFunction(data);
+        }
       });
     } else {
       $("#search_btn_id").addClass("disabled");
     }
-    
-    function process(data) {
-      if (func === "year") {
-        years(data);
-      } else if (func === "year_make") {
-        year_make(data);
-      } else if (func === "year_make_model") {
-        year_make_model(data);
-      }
-    }
-    
-    function years(data) {
-      $("#results").html("");
-      
-      $("<tr>").html("<td><strong>year</strong></td><td><strong>make</strong></td>").appendTo("#results");
-      
-      var results = data["Results"];
-      $.each(results, function(key, val) {
-        console.log(val);
-        
-        $("<tr>").html(
-          "<td>" + val["ModelYear"] + "</td>"
-          +
-          "<td>" + val["Make"] + "</td>"
-          //+
-          //"<td>" + val["VehicleId"] + "</td>"
-          +
-          "<td></td>"
-          +
-          "<td></td>"
-        ).appendTo($("#results"));
-      });
-    }
-    
-    function year_make(data) {
-      $("#results").html("");
-      
-      $("<tr>").html("<td><strong>year</strong></td><td><strong>make</strong></td><td><strong>model</strong></td>").appendTo("#results");
-      
-      var results = data["Results"];
-      $.each(results, function(key, val) {
-        console.log(val);
-        
-        $("<tr>").html(
-          "<td>" + val["ModelYear"] + "</td>"
-          +
-          "<td>" + val["Make"] + "</td>"
-          +
-          "<td>" + val["Model"] + "</td>"
-          +
-          "<td></td>"
-          +
-          "<td></td>"
-        ).appendTo($("#results"));
-      });
-    }
-    
-    function year_make_model(data) {
-      $("#results").html("");
-      
-      $("<tr>").html("<td><strong>id</strong></td>" +
-                    "<td>&nbsp;&nbsp;</td>" +
-                    "<td><strong>year/make/model</strong></td>"
-                    ).appendTo("#results");
-      
-      var results = data["Results"];
-      $.each(results, function(key, val) {
-        console.log(val);
-        
-        $("<tr>").html(
-          "<td>" + val["VehicleId"] + "</td>"
-          +
-          "<td>&nbsp;&nbsp;</td>"
-          +
-          "<td>" + val["VehicleDescription"] + "</td>"
-        ).appendTo($("#results"));
-      });
-    }
   });
-  
+
+
   $("#search_btn_rating").click(function(e) {
     e.preventDefault();
-	
-    var $vehicle_id = $(":input[name=vehicle_id]").val();
-    
-    if (!($vehicle_id === "")) {
+
+    var vehicle_id = $(":input[name=vehicle_id]").val().squish();
+
+    if (vehicle_id !== "") {
+      $("#search_btn_rating").removeClass("disabled");
+
       $.ajax({
-          type: "GET",
-          url: "http://www.nhtsa.gov/webapi/api/SafetyRatings/vehicleID/" + $vehicle_id + "?format=json",
-          dataType: "jsonp",
-          success: process
+        type: "GET",
+        url: "http://www.nhtsa.gov/webapi/api/SafetyRatings/vehicleID/" + vehicle_id + "?format=json",
+        dataType: "jsonp",
+        success: function (data) {
+          rating(data);
+        }
       });
     } else {
       $("#search_btn_rating").addClass("disabled");
     }
-    
-    function process(data) {
-      $("#results").html("");
-      
-      $("<tr>").html("<td><strong>year/make/model</strong></td>" +
-                    "<td>&nbsp;&nbsp;</td>" +
-                     "<td><strong>rating</strong></td>"
-                  ).appendTo("#results");
-      
-      var results = data["Results"];
-      $.each(results, function(key, val) {
-        console.log(val);
-        
-        $("<tr>").html(
-          "<td>" + val["VehicleDescription"] + "</td>"
-          +
-          "<td>&nbsp;&nbsp;</td>"
-          +
-          "<td>" + val["OverallRating"] + "</td>"
-        ).appendTo($("#results"));
-      });
-    }
   });
-  
-  $("#search_btn_id").addClass('disabled');
-};
 
-$(document).ready(main);
+});
